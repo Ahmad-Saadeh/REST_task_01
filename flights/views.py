7cca42be605fca4880e790b59689696ba0f01629
Ahmad-Saadeh <Ahmad-Saadeh@users.noreply.github.com>
2020-02-23 14:43:15 +0200

    task_01

--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,13 @@
+from datetime import datetime
 from .models import Flight, Booking
+from .serializers import FlightSerializer,BookingSerializer
+from rest_framework.generics import ListAPIView
+
+class Flightlist(ListAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class Bookinglist(ListAPIView):
+    queryset = Booking.objects.filter(date__gt=datetime.today())
+    serializer_class = BookingSerializer
